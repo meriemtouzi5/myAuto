a94852dbcf9bccb1db4e6450259bc71e68a25d85
touzimobiblanc <touzi@mobiblanc.com>
2024-12-09 16:00:11 +0100

    change version sass-loader  and config webpack

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Header from './components/Header';
 function App() {
     return (
         <div>
+            <Header />
             <h1>MyAuto</h1>
         </div>
     );
